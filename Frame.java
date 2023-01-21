d8fd4dfa11fbee9d1b7a98dcb9959154c6d5e26e
Dangaga <121012200+dangaga0@users.noreply.github.com>
2023-01-21 15:51:27 +0100

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+
+public class Frame extends JFrame{
+
+
+    Frame(){
+
+        this.setSize(420,420);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Galculator");
+
+
+    }
+
+    
+}
---
+++ enemigo.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+
+public class enemigo {
+
+    static Random rm = new Random();
+
+    static String name;
+    static boolean turn = false; 
+    static int hp;
+    static int dm;
+    static boolean vivo = true;
+    static boolean com = false;
+    static int price;
+
+    enemigo(String name,int hp,int dm,int price){
+
+        this.name = name;
+        this.hp = hp;
+        this.dm = dm;
+        this.price = price;
+
+        
+    }
+
+    public static void pato(){
+
+        enemigo pato = new enemigo("pato",7,2,2);
+        com = true;
+        if(objetos.escudo.sold){
+            pato.dm = pato.dm -1;
+        }
+
+    }
+
+    public static void serpiente(){
+
+        enemigo serpiente = new enemigo("serpien",11,4,6);
+        com = true;
+        if(objetos.escudo.sold){
+            serpiente.dm = serpiente.dm -1;
+        }
+
+    }
+
+    public static void dragon(){
+
+        enemigo dragon = new enemigo("dragon",25,7,12);
+        com = true;
+        if(objetos.escudo.sold){
+            dragon.dm = dragon.dm -1;
+        }
+
+    }
+
+
+    public static void ataque(){
+
+        System.out.println("el enemigo a atacado de vulta infringiendo " + enemigo.dm);
+        personaje.hp = (personaje.hp - enemigo.dm);
+        System.out.println("--------------------------------\ntu\thp\tdaño\ntu\t"+ personaje.hp+"\t"+personaje.dm+"\n----------------------------------------");
+        
+    }
+
+    
+}
+
